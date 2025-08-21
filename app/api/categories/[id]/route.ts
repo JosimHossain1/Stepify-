9342959aa69471934f8c5b852b5c72e7f11d9d44
JosimHossain1 <josimhossain842@gmail.com>
2025-08-21 22:42:59 +0600

    Category API Endpoint develop

--- app/api/categories/[id]/route.ts
+++ app/api/categories/[id]/route.ts
@@ -2,20 +2,88 @@ import dbConnection from '@/lib/dbConnect';
 import CategoryModel from '@/models/CategoryModel';
 import { NextRequest } from 'next/server';
 
-export async function DELETE(
-  _request: NextRequest,
+// GET single Category
+export async function GET(
+  req: NextRequest,
   { params }: { params: { id: string } },
 ) {
   const { id } = params;
   await dbConnection();
+  try {
+    const singleCategory = await CategoryModel.findById(id);
+    return new Response(
+      JSON.stringify({
+        success: true,
+        message: 'Category Goted',
+        data: singleCategory,
+      }),
+      { status: 200 },
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        success: false,
+        message: error,
+      }),
+      { status: 404 },
+    );
+  }
+}
 
-  await CategoryModel.findByIdAndDelete(id);
+// PUT - Update single Category data
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  await dbConnection();
+  const { id } = params;
 
-  return new Response(
-    JSON.stringify({
-      success: true,
-      message: 'Category Deleted Successfully!',
-    }),
-    { status: 200 },
-  );
+  try {
+    const updatedCategory = await CategoryModel.findByIdAndUpdate(id);
+    return new Response(
+      JSON.stringify({
+        success: true,
+        message: 'Category Updated Successfully!',
+        data: updatedCategory,
+      }),
+      { status: 200 },
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        success: false,
+        message: error,
+      }),
+      { status: 500 },
+    );
+  }
+}
+
+// DELETE - Delete single Category by id
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  const { id } = params;
+
+  await dbConnection();
+
+  try {
+    await CategoryModel.findByIdAndDelete(id);
+    return new Response(
+      JSON.stringify({
+        success: true,
+        message: 'Category Deleted Successfully!',
+      }),
+      { status: 200 },
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        success: true,
+        message: error,
+      }),
+      { status: 500 },
+    );
+  }
 }
